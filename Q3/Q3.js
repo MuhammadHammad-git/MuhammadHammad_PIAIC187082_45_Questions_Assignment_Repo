2c95237021dbef65fa084739751813c4a604a7c6
Muhammad Hammad EE <muhammadhammad.javeed0@gmail.com>
2024-02-24 23:11:15 +0500

    Done Till Q5

---
+++ Q3/Q3.js
@@ -0,0 +1,5 @@
+"use strict";
+let personQ3 = "Hammad";
+console.log("Name in lowercase is", personQ3.toLowerCase(), "\n"
+    + "Name in UPPERCASE is", personQ3.toUpperCase(), "\n"
+    + "Name in Titlecase is", personQ3.charAt(0).toUpperCase() + personQ3.slice(1).toLowerCase());
