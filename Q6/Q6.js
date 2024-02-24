a81ff6f4042a25b4eb0abda016ef21c6bc44b5d5
Muhammad Hammad EE <muhammadhammad.javeed0@gmail.com>
2024-02-24 23:24:52 +0500

    Done till Q7

---
+++ Q6/Q6.js
@@ -0,0 +1,4 @@
+"use strict";
+let personQ6 = "\n\n\t\t\tMuhammad Hammad\t\t\n\n";
+console.log("Person's name before stripping \"" + personQ6 + "\"");
+console.log("Person's name after stripping \"" + personQ6.trim() + "\"");
