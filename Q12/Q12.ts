4c31c5cee326fcab74ab875cdffb123533f0c622
Muhammad Hammad EE <muhammadhammad.javeed0@gmail.com>
2024-03-16 12:27:20 +0500

    Commit

--- Q12/Q12.ts
+++ Q12/Q12.ts
@@ -1,8 +1,8 @@
 let namesarray = ["Hammad", "Ali", "Ahmed", "Kashif", "Khan"];
 
 let messageforfriend = "How are you?";
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
-console.log(names[4]);
+console.log(messageforfriend, names[0]);
+console.log(messageforfriend, names[1]);
+console.log(messageforfriend, names[2]);
+console.log(messageforfriend, names[3]);
+console.log(messageforfriend, names[4]);
